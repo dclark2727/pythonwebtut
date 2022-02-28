eecdb646beed124d740f8bc5d1ae502687cd0bec
dclark2727 <57651544+dclark2727@users.noreply.github.com>
2022-02-27 20:34:31 -0600

    create

---
+++ views.py
@@ -0,0 +1,35 @@
+from flask import Blueprint, flash, jsonify, render_template, request
+from flask_login import login_required, current_user
+from .models import Note
+from . import db
+import json
+
+views = Blueprint('views', __name__)
+
+@views.route('/', methods=['GET', 'POST'])
+@login_required
+def home():
+    if request.method == 'POST':
+        note = request.form.get('note')
+
+        if len(note) < 1:
+            flash('Note is too short.', category='error')
+        else:
+            newNote = Note(data = note, userID=current_user.id)
+            db.session.add(newNote)
+            db.session.commit()
+            flash('Note added!', category='success')
+
+    return render_template("home.html", user=current_user)
+
+@views.route('/delete-note',methods=['POST'])
+def delete_note():
+    note = json.loads(request.data)
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.userID == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+            
+    return jsonify({})
